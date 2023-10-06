1215caed57a936db212f2d59da526a668f07ee8b
nicholassuwirman <wilsonsuwirmans@gmail.com>
2023-10-06 13:13:45 +0200

    Added mobile section

--- src/containers/hero/Hero.jsx
+++ src/containers/hero/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
   const Menu = () => {
     return (
       <>
-         <Link to="" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(0)} onMouseLeave={handleMouseLeave}>Home</Link>
-        <Link to="" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(1)} onMouseLeave={handleMouseLeave}>About</Link>
+        <Link to="home" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(0)} onMouseLeave={handleMouseLeave}>Home</Link>
+        <Link to="about" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(1)} onMouseLeave={handleMouseLeave}>About</Link>
         <Link to="" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(2)} onMouseLeave={handleMouseLeave}>Projects</Link>
         <Link to="" spy={true} smooth={true} duration={500} onMouseOver={() => handleMouseOver(3)} onMouseLeave={handleMouseLeave}>Contact</Link>
       </>
@@ -32,7 +32,7 @@ const Hero = () => {
   }
 
   return (
-    <div className="menu">
+    <div className="menu" id="home">
       <div className="menu-items">
        <Menu />
       </div>
