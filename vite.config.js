e409d79964ed5886478a5d5a2c0b19cc1dc2274f
Nicholas <wilsonsuwirmans@gmail.com>
2024-12-07 21:30:40 +0100

    transfering to vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Nicholas-Wilson-Suwirman-Portfolio/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this matches your build output folder
+  },
+});
